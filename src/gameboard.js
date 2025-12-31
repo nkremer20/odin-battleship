31c93ef1af804d15e5c9651395bd5cfd98acfda4
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-12-31 00:42:27 -0600

    feat: Created Cell class and method to create the initial gameboard object

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,18 +1,44 @@
-class Gameboard {
-    constructor(board) {
-        this.board = this.#createGameboard();
+class Cell {
+    constructor(cellStatus=0, cellID) {
+        // Status contains the current state of the cell
+        // 0 = water
+        // 1 = ship
+        // 2 = ship hit
+        // 3 = miss
+        this.cellStatus = cellStatus;
+        this.cellID = cellID;
     }
 
-    #createGameboard() {
-        return this.board = [
-            [1,1,1],
-            [1,1,1]
-        ]
+    updateStatus(newStatus) {
+        if (this.status !== newStatus) {
+            return this.status = newStatus;
+        }
+        
+        return
     }
 }
 
-// class Cell {
+class Gameboard {
 
-// }
+    board = this.#createGameboard()
+
+    #createGameboard() {
+        let matrix = [];
+        const rows_columns = 10;
+
+        for (let i = 0; i < rows_columns; i++) {
+            const row = [];
+            const char = String.fromCharCode(65 + i);
+
+            for (let j = 0; j < rows_columns; j++) {
+                const newCell = new Cell(0, char + (j + 1))
+                row.push(newCell)
+            }
+            matrix.push(row);
+        }
+
+        return matrix;
+    }
+}
 
 export { Gameboard }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './styles.css';
 import { Ship } from './ship';
 import { Gameboard } from './gameboard';
+
+
+const newBoard = new Gameboard()
+console.table(newBoard)
